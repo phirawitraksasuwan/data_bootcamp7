737d12ba4265b3bb0e3f9a8dd43c587778d4aaae
พีรวิชญ์ รักษาสุวรรณ <129496289+phirawitraksasuwan@users.noreply.github.com>
2023-06-03 20:51:32 +0700

    Update 03. Case Whan .R

--- R/Data Transformation/library(tidyverse)/03. Mutate/03. Case Whan .R
+++ R/Data Transformation/library(tidyverse)/03. Mutate/03. Case Whan .R
@@ -17,3 +17,23 @@ DF2 <- data.frame(id = 6:8,
                   country = c("Germany","Italy","Sweden"))
 DF3 <- data.frame(id = 9:10, 
            country = c("Canada","Malaysia"))
+
+
+# ความเเตกต่างระหว่าง R , SQL
+# R
+Full_df %>%
+  mutate(region = case_when(
+    country %in% c("Thailand","Korea", "Japan","Malaysia") ~ "Asia",
+    country %in% c("Canada", "USA" ) ~ "America",
+    TRUE ~ "Europw"
+  ))
+
+# SQL
+library(sqldf)
+sqldf("select * , case 
+        when country in ('USA','Canada') then 'America'
+        when country in ('Thailand','Korea','Japan','Malaysia') then 'Asia'
+        else 'Europe'
+        end as region
+        from Full_df
+      ")
