c1482056973c705868e9dc2b20380f6abffe9654
พีรวิชญ์ รักษาสุวรรณ <129496289+phirawitraksasuwan@users.noreply.github.com>
2023-06-03 19:45:27 +0700

    Create 03. FOR .R

---
+++ R/Control Flow and Function/03. FOR .R
@@ -0,0 +1,21 @@
+#for
+
+friends <- c("xo", "yeen", "peet")
+
+for (friend in friends) {
+  print(friend)
+
+[1] "xo"
+[1] "yeen"
+[1] "peet"
+
+-- ตัวเลข
+nums <- c(5 , 4, 34 ,20 ,9)
+nums <- num+2
+nums
+
+[1]  7  6 36 22 11
+
+## เทคนิค การใส่วงเล็บ ทำให้ไม่ต้องเขียน num เพื่อให้เเสดงผล เเต่มันเเสดงให้เลย 
+(num <- num-2)
+[1]  5  4 34 20  9
