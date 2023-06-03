38c288046461ea9e7b8990784af1b15c3353926d
พีรวิชญ์ รักษาสุวรรณ <129496289+phirawitraksasuwan@users.noreply.github.com>
2023-06-03 22:05:42 +0700

    Update Correlation .R

--- R_Statistics/Correlation .R
+++ R_Statistics/Correlation .R
@@ -30,3 +30,20 @@ cor(mtcars$hp, mtcars$mpg)
 cor.test(mtcars$hp, mtcars$mpg) # Pearson's product-moment correlation 
 ## cor.test ค่าที่ต้องการออกมาหมด p-value ถ้า p-value < 0.05 ถือว่าใช้ได้
 ### confidence interval ช่วงความเชื่อมั่นโดยมั่นใจได้เเค่ 95% 
+
+######################################################################
+# ตัวอย่าง Multiple Correlation
+> cor(mtcars[ , c("mpg", "wt", "hp")])
+           mpg         wt         hp
+mpg  1.0000000 -0.8676594 -0.7761684
+wt  -0.8676594  1.0000000  0.6587479
+hp  -0.7761684  0.6587479  1.0000000
+
+# ตัวอย่าง Multiple Correlation อีก1วิธี โดย Pipes
+> mtcars %>%
++   select(mpg, wt, hp) %>% 
++   cor()
+           mpg         wt         hp
+mpg  1.0000000 -0.8676594 -0.7761684
+wt  -0.8676594  1.0000000  0.6587479
+hp  -0.7761684  0.6587479  1.0000000
