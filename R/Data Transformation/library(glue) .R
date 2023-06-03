30884c06cede5a397b4f2ee451655ba2be914f7a
พีรวิชญ์ รักษาสุวรรณ <129496289+phirawitraksasuwan@users.noreply.github.com>
2023-06-03 20:37:09 +0700

    Create library(glue) .R

---
+++ R/Data Transformation/library(glue) .R
@@ -0,0 +1,9 @@
+# glue กาว
+# string template
+
+my_name <- "toy"
+my_age <- 34
+
+glue("Hello my name is {my_name}")
+glue("Hello my name is {my_name}, and I'm {my_age} years old.")
+paste(" Hello my name ", my_name ,", and I'm ", my_age ," years old")
