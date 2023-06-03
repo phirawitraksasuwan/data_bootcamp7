ea7992c7e89b4bc827dfad1975973dce30717d13
พีรวิชญ์ รักษาสุวรรณ <129496289+phirawitraksasuwan@users.noreply.github.com>
2023-06-03 15:25:57 +0700

    Update 01.IF.R

--- R/Control Flow and Function/01.IF.R
+++ R/Control Flow and Function/01.IF.R
@@ -1,1 +1,14 @@
+## control flow
+## if = IF() in google sheet
 
+score <- 88 
+
+if (score >= 80){
+  print("Passed")
+}
+else if (score >= 50){
+  print("OK")
+}
+else {
+  print("Enroll again!")
+}
